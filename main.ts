591c4aad7508cbb99ad7a5d53c470ef69a90cdc3
Pantelis Ioannidis <ioannidis.pant.dev@gmail.com>
2022-01-21 19:47:17 +0200

    Αρχικά αρχεία για έργο MakeCode

---
+++ main.ts
@@ -0,0 +1,25 @@
+let Μετρητής = 0
+input.onButtonPressed(Button.A, function () {
+    Μετρητής = 9
+    basic.showNumber(Μετρητής)
+    Μετρητής += -1
+    basic.showNumber(Μετρητής)
+    Μετρητής += -1
+    basic.showNumber(Μετρητής)
+    Μετρητής += -1
+    basic.showNumber(Μετρητής)
+    Μετρητής += -1
+    basic.showNumber(Μετρητής)
+    Μετρητής += -1
+    basic.showNumber(Μετρητής)
+    Μετρητής += -1
+    basic.showNumber(Μετρητής)
+    Μετρητής += -1
+    basic.showNumber(Μετρητής)
+    Μετρητής += -1
+    basic.showNumber(Μετρητής)
+    Μετρητής += -1
+    basic.showNumber(Μετρητής)
+    basic.clearScreen()
+    music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
+})
